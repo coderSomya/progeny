4d5d1b64817a23b3f6c3595be718010f33e268e8
Somya <isomya13@gmail.com>
2026-05-31 01:25:22 +0530

    send and handle events

--- runner.py
+++ runner.py
@@ -1,7 +1,21 @@
 from bcc import BPF
 from pathlib import Path
 
+def process_event(cpu, data, size):
+    event = bpf["events"].event(data)
+    print(f"Process {event.comm.decode()} (PID: {event.pid}, PPID: {event.ppid}) called sys_clone")
+
+
 bpf_source = Path('probe.c').read_text()
-bpf = BPF(text=pbf_source).trace_print()
+bpf = BPF(text=bpf_source)
+
+bpf["events"].open_perf_buffer(process_event)
+print("Monitoring sys_clone events... Press Ctrl+C to exit.")
+
+while True:
+    try:
+        bpf.perf_buffer_poll()
+    except KeyboardInterrupt:
+        break
 
 
